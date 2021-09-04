3f04791f2075b05e038f9a62a6b5ee41572e5935
alex <fancn21th@aliyun.com>
2021-09-04 10:22:50 +0800

    feat: submit login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,16 +1,43 @@
 <template>
-  <form class="form-inline">
-    <label for="email">Email:</label>
-    <input type="email" id="email" placeholder="Enter email" name="email" />
-    <label for="pwd">Password:</label>
-    <input type="password" id="pwd" placeholder="Enter password" name="pswd" />
-    <label> <input type="checkbox" name="remember" /> Remember me </label>
-    <button type="submit">Submit</button>
+  <form class="form-inline" @submit.prevent="submit">
+    <label for="email">邮箱:</label>
+    <input
+      type="email"
+      id="email"
+      placeholder="Enter email"
+      name="email"
+      v-model="email"
+    />
+    <label for="pwd">密码:</label>
+    <input
+      type="password"
+      id="pwd"
+      placeholder="Enter password"
+      name="pswd"
+      v-model="pswd"
+    />
+    <label> <input type="checkbox" name="remember" /> 记住我 </label>
+    <button type="submit">提交</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: 'fancn21th@gmail.com',
+      pswd: 'abcd',
+    };
+  },
+  methods: {
+    submit() {
+      console.log('表单提交内容', {
+        email: this.email,
+        pswd: this.pswd,
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
