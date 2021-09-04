cfccb5ef0734cac97ba1528c1ea1863bcfef9e81
alex <fancn21th@aliyun.com>
2021-09-04 10:41:19 +0800

    feat: switch button added

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,19 +4,29 @@
     <input
       type="email"
       id="email"
-      placeholder="Enter email"
       name="email"
+      placeholder="Enter email"
       v-model="email"
     />
     <label for="pwd">密码:</label>
     <input
       type="password"
       id="pwd"
-      placeholder="Enter password"
       name="pswd"
+      placeholder="Enter password"
       v-model="pswd"
     />
-    <label> <input type="checkbox" name="remember" /> 记住我 </label>
+    <!-- Rounded switch -->
+    <label for="rememberme">记住我:</label>
+    <label class="switch">
+      <input
+        type="checkbox"
+        id="rememberme"
+        name="rememberme"
+        v-model="rememberme"
+      />
+      <span class="slider round"></span>
+    </label>
     <button type="submit">提交</button>
   </form>
 </template>
@@ -27,6 +37,7 @@ export default {
     return {
       email: 'fancn21th@gmail.com',
       pswd: 'abcd',
+      rememberme: false,
     };
   },
   methods: {
@@ -34,6 +45,7 @@ export default {
       console.log('表单提交内容', {
         email: this.email,
         pswd: this.pswd,
+        rememberme: this.rememberme,
       });
     },
   },
