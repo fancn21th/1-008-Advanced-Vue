a5def8ab2c0a6e5d959f27283b113d39ee9ef451
alex <fancn21th@aliyun.com>
2021-09-05 22:03:15 +0800

    refactor: refactor as comp

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,25 @@
   <div id="app">
     <div class="card">
       <div class="container">
-        <input :value="date" type="text" ref="input" placeholder="YYYY-MM-DD" />
+        from: <date-picker v-model="date" /> to: <date-picker v-model="date2" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Pikaday from 'pikaday';
-import '../node_modules/pikaday/css/pikaday.css';
+import DatePicker from './components/DatePicker.vue';
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    DatePicker,
+  },
   data() {
     return {
       date: '2021-10-01',
+      date2: '2021-10-01',
     };
-  },
-  mounted() {
-    const picker = new Pikaday({
-      field: this.$refs.input,
-      format: 'D/M/YYYY',
-      toString(date) {
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        return `${year}-${month}-${day}`;
-      },
-      parse(dateString) {
-        const parts = dateString.split('-');
-        const year = parseInt(parts[0], 10);
-        const month = parseInt(parts[1], 10) - 1;
-        const day = parseInt(parts[2], 10);
-        return new Date(year, month, day);
-      },
-      onSelect: () => {
-        this.date = picker.toString();
-      },
-    });
   },
 };
 </script>
---
+++ src/components/DatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <input :value="value" type="text" ref="input" placeholder="YYYY-MM-DD" />
+</template>
+
+<script>
+import Pikaday from 'pikaday';
+import '../../node_modules/pikaday/css/pikaday.css';
+
+export default {
+  props: {
+    value: String,
+  },
+  mounted() {
+    const picker = new Pikaday({
+      field: this.$refs.input,
+      format: 'D/M/YYYY',
+      toString(date) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${year}-${month}-${day}`;
+      },
+      parse(dateString) {
+        const parts = dateString.split('-');
+        const year = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const day = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+      },
+      onSelect: () => {
+        this.$emit('input', picker.toString());
+      },
+    });
+  },
+};
+</script>
+
+<style></style>
