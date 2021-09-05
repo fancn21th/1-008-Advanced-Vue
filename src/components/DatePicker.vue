33a5ea2e66d224fd687be47670e4b2d7acee324d
alex <fancn21th@aliyun.com>
2021-09-05 22:22:59 +0800

    fix: moment missing

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -13,20 +13,20 @@ export default {
   mounted() {
     const picker = new Pikaday({
       field: this.$refs.input,
-      format: 'D/M/YYYY',
-      toString(date) {
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        return `${year}-${month}-${day}`;
-      },
-      parse(dateString) {
-        const parts = dateString.split('-');
-        const year = parseInt(parts[0], 10);
-        const month = parseInt(parts[1], 10) - 1;
-        const day = parseInt(parts[2], 10);
-        return new Date(year, month, day);
-      },
+      format: 'YYYY-MM-DD',
+      // toString(date) {
+      //   const day = date.getDate();
+      //   const month = date.getMonth() + 1;
+      //   const year = date.getFullYear();
+      //   return `${year}-${month}-${day}`;
+      // },
+      // parse(dateString) {
+      //   const parts = dateString.split('-');
+      //   const year = parseInt(parts[0], 10);
+      //   const month = parseInt(parts[1], 10) - 1;
+      //   const day = parseInt(parts[2], 10);
+      //   return new Date(year, month, day);
+      // },
       onSelect: () => {
         this.$emit('input', picker.toString());
       },
