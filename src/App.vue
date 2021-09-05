f91c4b3de840b09cd8741b850222c4235a764009
alex <fancn21th@aliyun.com>
2021-09-05 21:34:39 +0800

    feat: introduce pikaday lib

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <div id="app">
-    <LoginForm />
+    <div class="card">
+      <div class="container">
+        <input :value="date" type="text" ref="input" placeholder="YYYY-MM-DD" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import LoginForm from './components/LoginForm.vue';
+import Pikaday from 'pikaday';
+import '../node_modules/pikaday/css/pikaday.css';
 
 export default {
   name: 'App',
-  components: {
-    LoginForm,
+  components: {},
+  data() {
+    return {
+      date: '2021-10-01',
+    };
+  },
+  mounted() {
+    const picker = new Pikaday({
+      field: this.$refs.input,
+      format: 'D/M/YYYY',
+      toString(date) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${year}-${month}-${day}`;
+      },
+      parse(dateString) {
+        const parts = dateString.split('-');
+        const year = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const day = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+      },
+      onSelect: () => {
+        this.date = picker.toString();
+      },
+    });
   },
 };
 </script>
